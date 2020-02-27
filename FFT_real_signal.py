1d0d5286a098ce5d335f22feb97d8d1ca43df7fa
MarkStudio <markchen8024@gmail.com>
2020-02-27 17:06:19 +0800

    add a python file to FFT a  real signal

---
+++ FFT_real_signal.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.fftpack import fft
+import sys, os, math
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../software/models/'))
+import utilFunctions as UF
+
+M = 501
+hM1 = int(math.floor((M+1)/2))
+hM2 = int(math.floor(M/2))
+
+(fs, x) = UF.wavread('../sounds/soprano-E4.wav')
+x1 = x[5000:5000+M] * np.hamming(M)
+
+N = 511
+fftbuffer = np.zeros(N)
+# center the signal into dftbuffer
+fftbuffer[:hM1] = x1[hM2:]
+fftbuffer[N-hM2:] = x1[:hM2]
+
+X = fft(fftbuffer)
+mX = abs(X)
+pX = np.unwrap(np.angle(X))
